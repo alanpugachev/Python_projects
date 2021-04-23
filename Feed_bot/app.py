4105631b442ca343e43b5bd93fd7de3a32d3096f
69Lmao420 <aszneesizzle@gmail.com>
2021-04-23 14:53:18 +0300

    Removed comments and trash code, removed some files for revision

--- Feed_bot/app.py
+++ Feed_bot/app.py
@@ -2,14 +2,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN
 
 
-def notifications_text_switcher(enable):
-    if enable:
-        return "disabled"
-    if not enable:
-        return "enabled"
-# !COMING SOON! notification switcher
-
-
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
@@ -26,14 +18,7 @@ async def process_help_command(message: types.Message):
     await bot.send_message(message.from_user.id, "*list of commands*", disable_notification=False)
 
 
-@dp.message_handler(commands=['notifications'])
-async def process_notifications_command(message: types.Message):
-    await bot.send_message(message.from_user.id, f"Notifications {notifications_text_switcher(False)}", disable_notification=False)
-
-
 @dp.message_handler()
-# If you do not specify the library of the
-# message being processed, then by default only processing text messages
 async def echo_message(message: types.Message):
     await bot.send_message(message.from_user.id, "To use the bot, use the commands.\nTo get a list of commands, use the command '/help'.")
 
