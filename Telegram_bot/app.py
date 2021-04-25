5b1ac6bfd10df4c3fb30b38b9507db3e0e80a023
69Lmao420 <aszneesizzle@gmail.com>
2021-04-25 15:01:30 +0300

    Change bot token

--- Telegram_bot/app.py
+++ Telegram_bot/app.py
@@ -9,18 +9,18 @@ dp = Dispatcher(bot)
 # handlers
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, "Hello!\nYou can send me telegram channel links and I can send you news posts from these chanells.",
-                           disable_notification=False)
+    await bot.send_message(message.from_user.id, "Hello!\nYou can send me telegram channel links and I can send you news posts from these chanells.")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await bot.send_message(message.from_user.id, "*list of commands*", disable_notification=False)
+    await bot.send_message(message.from_user.id, "*list of commands*")
 
 
 @dp.message_handler()
 async def echo_message(message: types.Message):
-    await bot.send_message(message.from_user.id, "To use the bot, use the commands.\nTo get a list of commands, use the command '/help'.")
+    await bot.send_message(message.from_user.id, "To use the bot, use the commands.\nTo get a list of commands, "
+                                                 "use the command '/help'.")
 
 
 #polling
